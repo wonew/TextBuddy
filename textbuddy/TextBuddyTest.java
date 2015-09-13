bd1208d6be3371931fa5f3531189d64ceeb7e372
junewonewlim <junelym1@gmail.com>
2015-09-14 01:47:35 +0800

    Written first test case for sort() method

--- textbuddy/TextBuddyTest.java
+++ textbuddy/TextBuddyTest.java
@@ -2,13 +2,39 @@ package textbuddy;
 
 import static org.junit.Assert.*;
 
+import java.util.ArrayList;
+
 import org.junit.Test;
 
 public class TextBuddyTest {
 
+	private static final String SORTED_TEXT_1 = "Apple";
+	private static final String SORTED_TEXT_2 = "Banana";
+	private static final String SORTED_TEXT_3 = "Carrot";
+	private static final String SORTED_TEXT_4 = "Donkey";
+	private static final String SORTED_TEXT_5 = "Zebra";
+	
+	
+
 	@Test
 	public void testIsSorted() {
-		fail("Not yet implemented");
+		
+		ArrayList<String> listToTest = new ArrayList<String>();
+		ArrayList<String> sortedList = new ArrayList<String>();
+		
+		sortedList.add(SORTED_TEXT_1);
+		sortedList.add(SORTED_TEXT_2);
+		sortedList.add(SORTED_TEXT_3);
+		sortedList.add(SORTED_TEXT_4);
+		sortedList.add(SORTED_TEXT_5);
+		
+		listToTest.add(SORTED_TEXT_5);
+		listToTest.add(SORTED_TEXT_4);
+		listToTest.add(SORTED_TEXT_3);
+		listToTest.add(SORTED_TEXT_2);
+		listToTest.add(SORTED_TEXT_1);
+		
+		assertEquals(sortedList, TextBuddy.sort(listToTest));
 	}
 
 }
